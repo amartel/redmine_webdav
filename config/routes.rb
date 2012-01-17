097d46ad1421998da788c00696cf987e25c22f71
Arnaud MARTEL <arnaud@aclnr.net>
2012-01-17 11:11:01 +0100

    compatibility with redmine trunk

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'webdav/:id/*path_info', :controller  => 'webdav', :action => 'webdav'
   map.connect 'webdav/*path_info', :controller  => 'webdav', :action => 'webdavnf'
 
+  map.connect 'projects/:id/webdav_settings/:action', :controller => 'webdav_settings'
 end
