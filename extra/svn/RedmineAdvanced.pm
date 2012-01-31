29f07c2872773ee4cf9722f31b53ad4f45640e87
Arnaud MARTEL <arnaud@aclnr.net>
2012-01-31 13:56:52 +0100

    Add: - support for git smarthttp - restrict access for admin users only from specified IP addresses - cache management (when possible) - compatible with bioproj plugin (to affect a role to anonymous or not_member users)

--- extra/svn/RedmineAdvanced.pm
+++ extra/svn/RedmineAdvanced.pm
@@ -102,6 +102,84 @@ S<them :>
 
 And you need to upgrade at least reposman.rb (after r860).
 
+=head1 GIT SMART HTTP SUPPORT
+
+Git's smart HTTP protocol (available since Git 1.7.0) will not work with the
+above settings. Redmine.pm normally does access control depending on the HTTP
+method used: read-only methods are OK for everyone in public projects and
+members with read rights in private projects. The rest require membership with
+commit rights in the project.
+
+However, this scheme doesn't work for Git's smart HTTP protocol, as it will use
+POST even for a simple clone. Instead, read-only requests must be detected using
+the full URL (including the query string): anything that doesn't belong to the
+git-receive-pack service is read-only.
+
+To activate this mode of operation, add this line inside your <Location /git>
+block:
+
+  RedmineGitSmartHttp yes
+
+Here's a sample Apache configuration which integrates git-http-backend with
+a MySQL database and this new option:
+
+   SetEnv GIT_PROJECT_ROOT /var/www/git/
+   SetEnv GIT_HTTP_EXPORT_ALL
+   ScriptAlias /git/ /usr/libexec/git-core/git-http-backend/
+   <Location /git>
+       Order allow,deny
+       Allow from all
+
+       AuthType Basic
+       AuthName Git
+       Require valid-user
+
+       PerlAccessHandler Apache::Authn::RedmineAdvanced::access_handler
+       PerlAuthenHandler Apache::Authn::RedmineAdvanced::authen_handler
+       # for mysql
+       RedmineDSN "DBI:mysql:database=redmine;host=127.0.0.1"
+       RedmineDbUser "redmine"
+       RedmineDbPass "xxx"
+       RedmineGitSmartHttp yes
+    </Location>
+
+Make sure that all the names of the repositories under /var/www/git/ have a
+matching identifier for some project: /var/www/git/myproject and
+/var/www/git/myproject.git will work. You can put both bare and non-bare
+repositories in /var/www/git, though bare repositories are strongly
+recommended. You should create them with the rights of the user running Redmine,
+like this:
+
+  cd /var/www/git
+  sudo -u www-data mkdir myproject
+  cd myproject
+  sudo -u www-data git init --bare
+
+Once you have activated this option, you have three options when cloning a
+repository:
+
+- Cloning using "http://user@host/git/repo(.git)" works, but will ask for the password
+  all the time.
+
+- Cloning with "http://user:pass@host/git/repo(.git)" does not have this problem, but
+  this could reveal accidentally your password to the console in some versions
+  of Git, and you would have to ensure that .git/config is not readable except
+  by the owner for each of your projects.
+
+- Use "http://host/git/repo(.git)", and store your credentials in the ~/.netrc
+  file. This is the recommended solution, as you only have one file to protect
+  and passwords will not be leaked accidentally to the console.
+
+  IMPORTANT NOTE: It is *very important* that the file cannot be read by other
+  users, as it will contain your password in cleartext. To create the file, you
+  can use the following commands, replacing yourhost, youruser and yourpassword
+  with the right values:
+
+    touch ~/.netrc
+    chmod 600 ~/.netrc
+    echo -e "machine yourhost\nlogin youruser\npassword yourpassword" > ~/.netrc
+
+
 =cut
 
 use strict;
@@ -118,6 +196,7 @@ use Apache2::Access;
 use Apache2::ServerRec qw();
 use Apache2::RequestRec qw();
 use Apache2::RequestUtil qw();
+use Apache2::Connection qw();
 use Apache2::Const qw(:common :override :cmd_how);
 use APR::Pool  ();
 use APR::Table ();
@@ -178,10 +257,22 @@ my @directives = (
       args_how     => TAKE1,
       errmsg       => 'RedmineCacheCredsMax must be decimal number',
    },
+   {
+      name         => 'RedmineCheckAdmin',
+      req_override => OR_AUTHCFG,
+      args_how     => TAKE1,
+      errmsg => 'regexp matched by IP address for admin user to be granted',
+   },
+   {
+      name         => 'RedmineGitSmartHttp',
+      req_override => OR_AUTHCFG,
+      args_how     => TAKE1,
+   },
 );
 
 sub RedmineProjectId          { set_val( 'RedmineProjectId',          @_ ); }
 sub RedmineAuthenticationOnly { set_val( 'RedmineAuthenticationOnly', @_ ); }
+sub RedmineCheckAdmin         { set_val( 'RedmineCheckAdmin',         @_ ); }
 
 sub RedmineReadPermissions {
    my ( $self, $parms, $arg ) = @_;
@@ -228,10 +319,22 @@ sub RedmineCacheCredsMax {
    }
 }
 
+sub RedmineGitSmartHttp {
+   my ( $self, $parms, $arg ) = @_;
+   $arg = lc $arg;
+
+   if ( $arg eq "yes" || $arg eq "true" ) {
+      $self->{RedmineGitSmartHttp} = 1;
+   }
+   else {
+      $self->{RedmineGitSmartHttp} = 0;
+   }
+}
+
 sub printlog {
    my $string = shift;
    my $r      = shift;
-   $r->log->debug($string);
+   $r->log->error($string);
 }
 
 sub trim {
@@ -249,6 +352,24 @@ Apache2::Module::add( __PACKAGE__, \@directives );
 
 my %read_only_methods = map { $_ => 1 } qw/GET PROPFIND REPORT OPTIONS/;
 
+sub request_is_read_only {
+  my ($r) = @_;
+  my $cfg = Apache2::Module::get_config(__PACKAGE__, $r->server, $r->per_dir_config);
+
+  # Do we use Git's smart HTTP protocol, or not?
+  if (defined $cfg->{RedmineGitSmartHttp} and $cfg->{RedmineGitSmartHttp}) {
+    my $uri = $r->unparsed_uri;
+    my $location = $r->location;
+    my $is_read_only = $uri !~ m{^$location/*[^/]+/+(info/refs\?service=)?git\-receive\-pack$}o;
+    return $is_read_only;
+  } else {
+    # Old behaviour: check the HTTP method
+    my $method = $r->method;
+    return defined $read_only_methods{$method};
+  }
+}
+
+
 sub access_handler {
    my $r = shift;
 
@@ -264,22 +385,34 @@ sub access_handler {
      if $cfg->{RedmineAuthenticationOnly};    #anonymous access not allowed
 
    #check public project AND anonymous access is allowed
+   my $project_id = get_project_identifier($r);
+   my $ano_access = get_redmine_cache( "", $project_id, $r, $cfg );
+   if ( defined $ano_access ) {
+      $r->set_handlers( PerlAuthenHandler => [ \&OK ] )
+        if ( ( $ano_access + 0 ) > 0 );       #anonymous access allowed
+      return OK;
+   }
+
    if ( !anonymous_denied($r) ) {
-      my $project_id = get_project_identifier($r);
       my $project_pub = is_public_project( $project_id, $r );
       if ( $project_pub < 0 ) {
 
          #Unknown project => only read access is granted
          $r->set_handlers( PerlAuthenHandler => [ \&OK ] )
-           if
-            defined $read_only_methods{ $r->method };  #anonymous access allowed
+           if request_is_read_only($r)
+            #defined $read_only_methods{ $r->method };  #anonymous access allowed
       }
       elsif ( $project_pub > 0 ) {
 
          #public project, so we check anonymous permissions
-         my $perm = get_anonymous_permissions($r);
-         $r->set_handlers( PerlAuthenHandler => [ \&OK ] )
-           if check_permission( $perm, $cfg, $r );     #anonymous access allowed
+         my $perm = get_anonymous_permissions( $r, $project_id );
+         if ( check_permission( $perm, $cfg, $r ) ) {
+            update_redmine_cache( "", $project_id, "1", $r, $cfg );
+            $r->set_handlers( PerlAuthenHandler => [ \&OK ] );
+         }
+         else {
+            update_redmine_cache( "", $project_id, "-1", $r, $cfg );
+         }
       }
    }
 
@@ -296,17 +429,23 @@ sub authen_handler {
 
    my $cfg =
      Apache2::Module::get_config( __PACKAGE__, $r->server, $r->per_dir_config );
+
    my $project_id = get_project_identifier($r);
 
-   #1. Check cache if available
-   my $usrprojpass;
-   my $pass_digest = Digest::SHA1::sha1_hex($redmine_pass);
-   if ( $cfg->{RedmineCacheCredsMax} ) {
-      $usrprojpass =
-        $cfg->{RedmineCacheCreds}->get( $r->user . ":" . $project_id );
-      return OK
-        if ( defined $usrprojpass and ( $usrprojpass eq $pass_digest ) );
+   #0. Check admin IP adresse if requested
+   if ( $cfg->{RedmineCheckAdmin} && $r->user =~ m/^admin$/i ) {
+      my $ipreg = $cfg->{RedmineCheckAdmin};
+      if ( !( $r->connection->remote_ip =~ m/($ipreg)/ ) ) {
+         $r->note_auth_failure();
+         return AUTH_REQUIRED;
+      }
    }
+
+   #1. Check cache if available
+   my $pass_digest = Digest::SHA1::sha1_hex($redmine_pass);
+   my $usrprojpass = get_redmine_cache( $r->user, $project_id, $r, $cfg );
+   return OK
+     if ( defined $usrprojpass and ( $usrprojpass eq $pass_digest ) );
 
    #2. Then authenticate user
    if ( !authenticate_user( $r->user, $redmine_pass, $r ) ) {
@@ -324,6 +463,7 @@ sub authen_handler {
    #if project doesn't exist then administrator is required
    if ( $project_pub < 0 ) {
       if ( is_admin( $r->user, $r ) ) {
+         update_redmine_cache( $r->user, $project_id, $pass_digest, $r, $cfg );
          return OK;
       }
       else {
@@ -337,8 +477,9 @@ sub authen_handler {
    if (@perms) {
       for my $perm (@perms) {
          if ( check_permission( $perm, $cfg, $r ) ) {
-            update_redmine_cache( $r->user, $project_id, $pass_digest, $cfg );
-            return OK if check_permission( $perm, $cfg, $r );
+            update_redmine_cache( $r->user, $project_id, $pass_digest, $r,
+               $cfg );
+            return OK;
          }
       }
 
@@ -346,8 +487,11 @@ sub authen_handler {
    elsif ( $project_pub > 0 ) {
 
       #public project so we check permissions for non-members
-      my $perm2 = get_nonmember_permission($r);
-      return OK if check_permission( $perm2, $cfg, $r );
+      my $perm2 = get_nonmember_permission( $r, $project_id );
+      if ( check_permission( $perm2, $cfg, $r ) ) {
+         update_redmine_cache( $r->user, $project_id, $pass_digest, $r, $cfg );
+         return OK;
+      }
    }
 
    $r->note_auth_failure();
@@ -403,6 +547,9 @@ sub get_project_identifier {
    else {
       my $location = $r->location;
       ($identifier) = $r->uri =~ m{$location/*([^/]+)};
+      if (defined $cfg->{RedmineGitSmartHttp} and $cfg->{RedmineGitSmartHttp}) {
+         $identifier =~ s/\.git//;
+      }
    }
 
    $identifier;
@@ -418,12 +565,37 @@ sub connect_database {
 }
 
 sub get_anonymous_permissions {
-   my $r = shift;
+   my $r          = shift;
+   my $project_id = shift;
 
    my $dbh = connect_database($r);
-   my $sth = $dbh->prepare("SELECT permissions FROM roles WHERE roles.id=2");
 
-   $sth->execute();
+   #Test if plugin BIOPROJ redefines permission for anonymous access...
+   my $sthrole = $dbh->prepare(
+"select project_id from enabled_modules INNER JOIN projects ON projects.id = enabled_modules.project_id AND projects.identifier= ? WHERE enabled_modules.name = 'bioproj' ;"
+   );
+
+   $sthrole->execute($project_id);
+   my $role = 2;
+   if ( my @row = $sthrole->fetchrow_array ) {
+      my $sthroleid = $dbh->prepare(
+         "select anonymous_role_id from bioproj_settings WHERE project_id = ?;"
+      );
+
+      $sthroleid->execute( $row[0] );
+
+      if ( my @row2 = $sthroleid->fetchrow_array ) {
+         if ( $row2[0] > 0 ) {
+            $role = $row2[0] - 2;
+         }
+      }
+      $sthroleid->finish();
+   }
+   $sthrole->finish();
+
+   my $sth = $dbh->prepare("SELECT permissions FROM roles WHERE roles.id=?");
+
+   $sth->execute($role);
    my ($ret) = $sth->fetchrow_array;
    $sth->finish();
    $dbh->disconnect();
@@ -443,10 +615,9 @@ sub authenticate_user {
      Apache2::Module::get_config( __PACKAGE__, $r->server, $r->per_dir_config );
 
    my $dbh = connect_database($r);
-   my $sth =
-     $dbh->prepare(
+   my $sth = $dbh->prepare(
 "SELECT hashed_password, salt, auth_source_id, id FROM users WHERE users.status=1 AND login=? "
-     );
+   );
 
    $sth->execute($redmine_user);
    while ( my ( $hashed_password, $salt, $auth_source_id, $user_id ) =
@@ -455,16 +626,15 @@ sub authenticate_user {
 
       #Check authentication
       unless ($auth_source_id) {
-         my $salted_password = Digest::SHA1::sha1_hex($salt.$pass_digest);
+         my $salted_password = Digest::SHA1::sha1_hex( $salt . $pass_digest );
          if ( $hashed_password eq $salted_password ) {
             $ret = 1;
          }
       }
       elsif ($CanUseLDAPAuth) {
-         my $sthldap =
-           $dbh->prepare(
+         my $sthldap = $dbh->prepare(
 "SELECT host,port,tls,account,account_password,base_dn,attr_login from auth_sources WHERE id = ?;"
-           );
+         );
          $sthldap->execute($auth_source_id);
          while ( my @rowldap = $sthldap->fetchrow_array ) {
             my $ldap = Authen::Simple::LDAP->new(
@@ -481,9 +651,6 @@ sub authenticate_user {
               if ( $ldap->authenticate( $redmine_user, $redmine_pass ) );
          }
          $sthldap->finish();
-      }
-      if ( $cfg->{RedmineFailedAttempts} ) {
-         failed_attempts( $user_id, $ret, $cfg, $r );
       }
    }
    $sth->finish();
@@ -508,6 +675,7 @@ sub is_admin {
    $ret;
 }
 
+
 sub check_permission() {
    my $perm = shift;
    my $cfg  = shift;
@@ -515,21 +683,24 @@ sub check_permission() {
 
    my $ret;
    my @listperms;
-   if ( defined $read_only_methods{ $r->method } ) {
-      @listperms =
-          $cfg->{RedmineReadPermissions}
-        ? @{ $cfg->{RedmineReadPermissions} }
-        : (':browse_repository');
-   }
-   else {
-      @listperms =
-          $cfg->{RedmineWritePermissions}
-        ? @{ $cfg->{RedmineWritePermissions} }
-        : (':commit_access');
-   }
-   for my $p (@listperms) {
-      if ( $perm =~ /$p/ ) {
-         $ret = 1;
+   if ( defined $perm ) {
+      if ( request_is_read_only($r)) {
+      #if ( defined $read_only_methods{ $r->method } ) {
+         @listperms =
+           $cfg->{RedmineReadPermissions}
+           ? @{ $cfg->{RedmineReadPermissions} }
+           : (':browse_repository');
+      }
+      else {
+         @listperms =
+           $cfg->{RedmineWritePermissions}
+           ? @{ $cfg->{RedmineWritePermissions} }
+           : (':commit_access');
+      }
+      for my $p (@listperms) {
+         if ( $perm =~ /$p/ ) {
+            $ret = 1;
+         }
       }
    }
    $ret;
@@ -557,12 +728,37 @@ sub get_user_permissions {
 }
 
 sub get_nonmember_permission {
-   my $r = shift;
+   my $r          = shift;
+   my $project_id = shift;
 
    my $dbh = connect_database($r);
-   my $sth = $dbh->prepare("SELECT permissions FROM roles WHERE roles.id=1");
 
-   $sth->execute();
+   #Test if plugin BIOPROJ redefines permission for anonymous access...
+   my $sthrole = $dbh->prepare(
+"select project_id from enabled_modules INNER JOIN projects ON projects.id = enabled_modules.project_id AND projects.identifier= ? WHERE enabled_modules.name = 'bioproj' ;"
+   );
+
+   $sthrole->execute($project_id);
+   my $role = 1;
+   if ( my @row = $sthrole->fetchrow_array ) {
+      my $sthroleid = $dbh->prepare(
+         "select non_member_role_id from bioproj_settings WHERE project_id = ?;"
+      );
+
+      $sthroleid->execute( $row[0] );
+
+      if ( my @row2 = $sthroleid->fetchrow_array ) {
+         if ( $row2[0] > 0 ) {
+            $role = $row2[0] - 2;
+         }
+      }
+      $sthroleid->finish();
+   }
+   $sthrole->finish();
+
+   my $sth = $dbh->prepare("SELECT permissions FROM roles WHERE roles.id=?");
+
+   $sth->execute($role);
    my ($ret) = $sth->fetchrow_array;
    $sth->finish();
    $dbh->disconnect();
@@ -574,27 +770,71 @@ sub update_redmine_cache {
    my $redmine_user = shift;
    my $project_id   = shift;
    my $redmine_pass = shift;
+   my $r            = shift;
    my $cfg          = shift;
 
-   my $pass_digest = Digest::SHA1::sha1_hex($redmine_pass);
-   if ( $cfg->{RedmineCacheCredsMax} ) {
-      my $usrprojpass =
-        $cfg->{RedmineCacheCreds}->get( $redmine_user . ":" . $project_id );
-      if ( defined $usrprojpass ) {
-         $cfg->{RedmineCacheCreds}
-           ->set( $redmine_user . ":" . $project_id, $pass_digest );
-      }
-      else {
-         if ( $cfg->{RedmineCacheCredsCount} < $cfg->{RedmineCacheCredsMax} ) {
+   my $access = "W";
+   #$access = "R" if ( defined $read_only_methods{ $r->method } );
+   $access = "R" if ( request_is_read_only($r) );
+   
+
+   my $cdate = time;
+
+   if (   ( defined $redmine_user )
+      and ( defined $project_id )
+      and ( defined $access ) )
+   {
+      my $key = $redmine_user . ":" . $project_id . ":" . $access;
+      if ( $cfg->{RedmineCacheCredsMax} ) {
+         my $value = $cfg->{RedmineCacheCreds}->get($key);
+         if ( defined $value ) {
             $cfg->{RedmineCacheCreds}
-              ->set( $redmine_user . ":" . $project_id, $pass_digest );
-            $cfg->{RedmineCacheCredsCount}++;
+              ->set( $key, $cdate . ":::" . $redmine_pass );
          }
          else {
-            $cfg->{RedmineCacheCreds}->clear();
-            $cfg->{RedmineCacheCredsCount} = 0;
+            if ( $cfg->{RedmineCacheCredsCount} < $cfg->{RedmineCacheCredsMax} )
+            {
+               $cfg->{RedmineCacheCreds}
+                 ->set( $key, $cdate . ":::" . $redmine_pass );
+               $cfg->{RedmineCacheCredsCount}++;
+            }
+            else {
+               $cfg->{RedmineCacheCreds}->clear();
+               $cfg->{RedmineCacheCredsCount} = 0;
+            }
          }
       }
+   }
+}
+
+sub get_redmine_cache {
+   my $redmine_user = shift;
+   my $project_id   = shift;
+   my $r            = shift;
+   my $cfg          = shift;
+
+   my $access = "W";
+   #$access = "R" if ( defined $read_only_methods{ $r->method } );
+   $access = "R" if ( request_is_read_only($r) );
+
+   my $cdate = time;
+   if (   ( defined $redmine_user )
+      and ( defined $project_id )
+      and ( defined $access ) )
+   {
+      my $key = $redmine_user . ":" . $project_id . ":" . $access;
+      my $retpwd;
+
+      if ( $cfg->{RedmineCacheCredsMax} ) {
+         my $value = $cfg->{RedmineCacheCreds}->get($key);
+         if ( defined $value ) {
+            my @toks = split( /:::/, $value );
+            if ( ( $toks[0] + 5 ) > $cdate ) {
+               $retpwd = $toks[1];
+            }
+         }
+      }
+
    }
 }
 
@@ -602,15 +842,23 @@ sub anonymous_denied {
    my $r = shift;
 
    my $dbh = connect_database($r);
-   my $sth =
-     $dbh->prepare("SELECT value FROM settings WHERE name='login_required'");
+   my $sth = $dbh->prepare(
+      "SELECT value FROM settings where settings.name = 'login_required';");
 
    $sth->execute();
-   my ($ret) = $sth->fetchrow_array;
+   my $ret;
+   if ( my @row = $sth->fetchrow_array ) {
+      if ( $row[0] eq "1" || $row[0] eq "t" ) {
+         $ret = 1;
+      }
+   }
    $sth->finish();
-   $dbh->disconnect();
+   undef $sth;
 
+   $dbh->disconnect();
+   undef $dbh;
    $ret;
+
 }
 
 1;
