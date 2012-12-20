c11ed71c5fa3fc92ccc08b47e373bffe2ff128e4
Arnaud MARTEL <arnaud@aclnr.net>
2012-12-20 12:34:04 +0100

    use gem uuid (thx xelkano)

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 gem "unicode"
 gem "mime-types"
+gem "uuid"
--- lib/acts_as_webdav.rb
+++ lib/acts_as_webdav.rb
@@ -151,7 +151,7 @@ module Railsdav
 
         def webdav_lock
           resource = find_resource_by_path(@path_info)
-          raise NotFoundError unless resource
+          #raise NotFoundError unless resource
 
           locktype = "read"
           lockscope = "exclusive"
@@ -162,7 +162,8 @@ module Railsdav
             owner = User.current.login
           end
 
-          newlock=UUID.create.guid
+          #newlock=UUID.create.guid
+          newlock=UUID.generate
           s = out_of_date
           if !@lock.nil? and not s
             if @lock[:owner] == User.current.login
