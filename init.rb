f4ecb19295203d1e2a859cabdbd0c3544591a35b
Arnaud MARTEL <arnaud@aclnr.net>
2013-01-17 09:48:07 +0100

    release 0.5.2

--- init.rb
+++ init.rb
@@ -22,7 +22,7 @@ Redmine::Plugin.register :redmine_webdav do
   name 'WebDav plugin'
   author 'Arnaud Martel'
   description 'WebDav plugin for managing files inside projects'
-  version '0.5.1'
+  version '0.5.2'
   requires_redmine :version_or_higher => '2.1.0'
 
   settings :default => {'webdav_file_enabled' => 1, 'webdav_document_enabled' => 1, 'webdav_repository_enabled' => 1}, :partial => 'webdav_settings/settings'
